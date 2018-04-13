735ffb40b32e025c8b3f7d3bc97b3238d4cf1db7
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-04-13 03:14:55 +0300

    Update SummaryBikes.jsx

--- src/SummaryBikes.jsx
+++ src/SummaryBikes.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
+import { createFragmentContainer, graphql} from 'react-relay';
 
-class SummaryBikes extends React.Component {
+const SummaryBikes = ({totals}) => {
 
-  render() {
+    let count = totals.bikes;
+
     return (<div className="col-lg-4">
               <div className="card card-body">
                 <h6>Bikes</h6>
+                  <br />
+                  <p className="fs-28 fw-100">{count}</p>
+                  <div className="text-gray fs-12">
+                    <i className="ti-stats-down text-danger mr-1"></i>
+                    8% decrease from last hour
+                  </div>
               </div>
             </div>);
-  }
 
 }
 
-export default SummaryBikes;
+export default createFragmentContainer(SummaryBikes,
+graphql`
+  fragment SummaryBikes_totals on Camera
+  {
+    id
+    bikes
+  }
+`);
