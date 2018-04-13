f06c9c3a80e83eed4884b43ad5663c4eea852dc7
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-04-13 23:20:39 +0300

    Add files via upload

---
+++ src/seriesColors.js
@@ -0,0 +1,16 @@
+const seriesColors = [
+  {
+    color: 'rgba(220,220,220,0.2)',
+    auxColor: 'rgba(220,220,220,1)'
+  },
+  {
+    color: 'rgba(151,187,205,0.2)',
+    auxColor: 'rgba(151,187,205,1)'
+  },
+  {
+    color: 'rgba(72,72,72,0.2)',
+    auxColor: 'rgba(72,72,72,1)'
+  }
+];
+
+module.exports = seriesColors;
