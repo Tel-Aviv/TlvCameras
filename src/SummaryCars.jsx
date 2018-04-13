f55683b0787b208937ae9105375ed237e3ac17bd
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-04-13 03:14:23 +0300

    Update SummaryCars.jsx

--- src/SummaryCars.jsx
+++ src/SummaryCars.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
+import { createFragmentContainer, graphql} from 'react-relay';
 
-class SummaryCars extends React.Component {
+const SummaryCars = ({totals}) =>  {
 
-  render() {
+    let count = totals.cars;
+
     return (<div className="col-lg-4">
               <div className="card card-body">
-                <h6>Cars</h6>
-                <br />
-                <p className="fs-28 fw-100">112</p>
-                <div className="text-gray fs-12">
-                  <i className="ti-stats-up text-success mr-1" aria-hidden="true"></i>
-                </div>
+                  <h6>Cars</h6>
+                  <br />
+                  <p className="fs-28 fw-100">{count}</p>
+                  <div className="text-gray fs-12">
+                    <i className="ti-stats-up text-success mr-1" aria-hidden="true"></i>
+                    12% increase from last day
+                  </div>
               </div>
             </div>);
-  }
+
 
 }
 
-export default SummaryCars;
+export default createFragmentContainer(SummaryCars,
+graphql`
+  fragment SummaryCars_totals on Camera
+  {
+    id
+    cars
+  }
+`);
