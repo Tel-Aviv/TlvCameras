e62f628ef8af5a0e54702988b2e6e0881b78f49d
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-04-15 01:37:17 +0300

    Add files via upload

---
+++ src/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { connect } from 'react-redux'
+
+const Header = ({cameraId, cameraName}) => {
+
+    let _cameraId = cameraId != 0 ?
+                    <div>{cameraId}</div> :
+                    null;
+
+    if( cameraName == '' ) {
+      cameraName = "Please, select a camera on the map";
+    }
+
+    return (<header className='topbar'>
+              <div className='topbar-left'>
+                 <div>{cameraName}</div>
+                 <div>{_cameraId}</div>
+              </div>
+            </header>
+            );
+
+};
+
+function mapStateToProps(state) {
+
+  return {
+    cameraId: state.cameraId,
+    cameraName: state.cameraName
+  }
+
+}
+
+export default connect(mapStateToProps)(Header);
