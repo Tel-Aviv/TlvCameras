73a3f815cf2737c41194dcb69e13439c4c4da8c5
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-04-14 22:12:25 +0300

    Update client.jsx

--- client.jsx
+++ client.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './src/reducers';
+
+let store = createStore(reducers,
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
 
 import App from './src/App'
 
-ReactDOM.render(<App />,
+ReactDOM.render(<Provider store={store}>
+                  <App />
+                </Provider>,
                 document.getElementById('root'));
