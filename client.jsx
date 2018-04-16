bc80f964aff41d94586332e1dc9f09ab7d979b24
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-04-17 01:44:01 +0300

    Add files via upload

--- client.jsx
+++ client.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { HashRouter as Router}  from 'react-router-dom';
 import reducers from './src/reducers';
+
+import Main from './src/Main'
 
 let store = createStore(reducers,
       window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 
-import App from './src/App'
-
 ReactDOM.render(<Provider store={store}>
-                  <App />
+                  <Router>
+                    <Main />
+                  </Router>
                 </Provider>,
-                document.getElementById('root'));
+document.getElementById('root'));
